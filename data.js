c90e60c99c2c25e29c763f582362c55452686a14
Дмитрий Канатников <steve111@yandex.ru>
2016-03-22 14:59:03 +0200

    info about me

---
+++ data.js
@@ -0,0 +1,60 @@
+module.exports = {
+    metrikaId: 20642695,
+    order: ['ru', 'en'],
+    favicons: {
+        ru: '//yastatic.net/morda-logo/i/favicon_islands.ico',
+        en: '//yastatic.net/morda-logo/i/favicon_comtr.ico'
+    },
+    cards: {
+        ru: {
+            lang: 'ru',
+            name: 'Дмитрий Канатников',
+            position: 'Разработчик интерфейсов',
+            address: {
+                country: 'Россия',
+                city: 'Москва',
+                zip: '119021',
+                'street-address': 'ул. Льва Толстого, д. 16'
+            },
+            company: {
+                name: 'Яндекс',
+                site: 'https://yandex.ru'
+            },
+            contact: {
+                work: '+7 (495) 739-70-00',
+                workExt: '4075',
+                cell: '+7 (965) 279-48-50',
+                email: 'rifler@yandex-team.ru',
+                site: 'https://rifler.github.io',
+                github: 'rifler',
+                twitter: 'dima_kanatnikov',
+                skype: 'rifler0989'
+            }
+        },
+        en: {
+            lang: 'en',
+            name: 'Dmitrii Kanatnikov',
+            position: 'Front-end developer',
+            address: {
+                country: 'Russia',
+                city: 'Moscow',
+                zip: '119021',
+                'street-address': '16, Leo Tolstoy St.'
+            },
+            company: {
+                name: 'Yandex',
+                site: 'https://yandex.com'
+            },
+            contact: {
+                work: '+7 (495) 739-70-00',
+                workExt: '4075',
+                cell: '+7 (965) 279-48-50',
+                email: 'rifler@yandex-team.ru',
+                site: 'https://rifler.github.io',
+                github: 'rifler',
+                twitter: 'dima_kanatnikov',
+                skype: 'rifler0989'
+            }
+        }
+    }
+};
